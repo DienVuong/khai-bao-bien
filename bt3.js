18d391244be1e223d27450588f9999fc910cfffe
DienVuong <vuongdien261@gmail.com>
2022-09-08 11:00:12 +0700

    them file

---
+++ bt3.js
@@ -0,0 +1,12 @@
+var a,b;
+a=+prompt( 'nhap so a')
+b=+prompt('nhap so b')
+
+/*document.write('tong hai so='+(a+b) )
+document.write('hieu 2 so'+ (a-b) )
+document.write('phan du khi chia 2 so'+(a%b) )
+document.write('tich hai so'+a*b)*/
+
+alert('tich hai so'+ a*b)
+alert('tong hai so' + (+a+b))
+alert('phan du khi chia 2 so' + a%b)
